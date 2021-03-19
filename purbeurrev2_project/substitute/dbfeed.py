8624d8d2b911e763d804d5fc1e085a39f7423bd4
screw-pack <gaillard.germain@gmail.com>
2021-03-19 09:30:29 +0100

    Created profile and product models

---
+++ purbeurrev2_project/substitute/dbfeed.py
@@ -0,0 +1,62 @@
+import requests
+
+from .models import Product
+
+
+class Dbfeed:
+    @staticmethod
+    def format_value(dict, key):
+        """ Checks if the field exist in the json from openfoodfacts API
+            and return a formated value"""
+
+        value = None
+
+        try:
+            value = dict[key]
+            if not value:
+                value = None
+
+            elif isinstance(value, list):
+                value = ', '.join(value)
+
+        except KeyError as error:
+            print("product {} doesn't have {} field".format(dict['code'],
+                                                            error))
+            print("set it to NULL")
+
+        return value
+
+    @classmethod
+    def feed(cls):
+        """ Returns datas from openfoodfacts API """
+
+        print('Querying datas...')
+        search = 'https://fr.openfoodfacts.org/cgi/search.pl?search_terms=&\
+        tagtype_0=states&tag_contains_0=contains&tag_0=checked&\
+        sort_by=unique_scans_n&page_size=20&page=1&json=1'
+
+        all = requests.get(search)
+        all = all.json()['products']
+
+        Product.objects.all().delete()
+
+        for entry in all:
+            product = Product(
+                name = cls.format_value(entry, 'product_name'),
+                brand = cls.format_value(entry, 'brands'),
+                tags = cls.format_value(entry, 'categories_tags'),
+                ingredients = cls.format_value(entry, 'ingredients_text_fr'),
+                additives = cls.format_value(entry, 'additives_tags'),
+                allergens = cls.format_value(entry, 'allergens_tags'),
+                nutriscore = cls.format_value(entry, 'nutriscore_grade'),
+                labels = cls.format_value(entry, 'labels'),
+                stores = cls.format_value(entry, 'stores_tags'),
+                link = cls.format_value(entry, 'url'),
+                compared_to = cls.format_value(entry, 'compared_to_category'),
+                image_url = cls.format_value(entry, 'image_url'))
+
+            product.save()
+
+
+if __name__ == "__main__":
+    Dbfeed.feed()
--- purbeurrev2_project/substitute/models.py
+++ purbeurrev2_project/substitute/models.py
@@ -1,5 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Favorite(models.Model):
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    brands = models.CharField(max_length=200)
+    tags = models.CharField(max_length=400)
+    ingredients = models.TextField()
+    additives = models.CharField(max_length=300, null=True, blank=True)
+    allergens = models.CharField(max_length=300, null=True, blank=True)
+    nutriscore = models.CharField(max_length=1, null=True, blank=True)
+    labels = models.CharField(max_length=300, null=True, blank=True)
+    stores = models.CharField(max_length=200, null=True)
+    link = models.CharField(max_length=200)
+    compared_to = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+    favorite = models.ManyToManyField(Product, related_name='profiles', blank=True)
+
+    def __str__(self):
+        return self.user.name
