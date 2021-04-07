7952dafeaef10894acc302268ed6f543603ea411
screw-pack <gaillard.germain@gmail.com>
2021-04-07 15:13:24 +0200

    added selenium test

--- substitute/forms.py
+++ substitute/forms.py
@@ -13,5 +13,6 @@ class ProductSearchForm(forms.Form):
     query = forms.CharField(label='Rechercher',
                             max_length=30,)
     query.widget.attrs.update({'class': 'form-control col-6',
+                               'id': 'search_input',
                                'content': 'text/html; charset=UTF-8',
                                'placeholder': 'Produit'})
