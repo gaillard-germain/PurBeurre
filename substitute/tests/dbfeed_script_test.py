0f935eefc2c5131aee11c9c857a8a91991b0568c
screw-pack <gaillard.germain@gmail.com>
2021-04-28 12:00:34 +0200

    update tests

---
+++ substitute/tests/dbfeed_script_test.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+
+from unittest.mock import patch
+
+from ..models import Product
+from ..scripts.dbfeed import Dbfeed
+
+
+class MockResponse:
+
+    def __init__(self):
+        self.status_code = 200
+
+    def json(self):
+        return {
+            "products": [{
+                "product_name": "Fake Product",
+                "brands": "Fake Brands",
+                "categories_tags": ["fake"],
+                "ingredients_text_fr": "fake",
+                "additives_tags": ["Efake"],
+                "allergens_tags": ["fake"],
+                "nutriscore_grade": "f",
+                "labels": "fake",
+                "stores_tags": ["Fake"],
+                "url": "https://fake",
+                "categories_hierarchy": ["fake1", "fake2", "fake3"],
+                "image_url": "https://fakeimage",
+                "_keywords": "fake"
+            }]
+        }
+
+
+class DBfeedTestCase(TestCase):
+
+    @patch('requests.get', return_value=MockResponse())
+    def test_dbfeed_add_product(self, mocked):
+        old_prod = Product.objects.count()
+        Dbfeed.feed(20, 1)
+        new_prod = Product.objects.count()
+        self.assertEqual(new_prod, old_prod+1)
---
+++ substitute/tests/selenium_test.py
@@ -0,0 +1,98 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+from ..models import Product
+
+
+class PurBeurreSeleniumTestCase(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.wait = WebDriverWait(self.driver, 1000)
+        self.addCleanup(self.driver.quit)
+
+        Product.objects.create(
+            name='fake product',
+            brands='fake brand',
+            tags='fake, test',
+            ingredients='fake, fake',
+            additives='E111, E222',
+            allergens='fake',
+            labels='fake label',
+            stores='super fake',
+            link="https://fake-product.html",
+            nutriscore="e",
+            image_url='http://fake.jpg',
+            keywords="fake keywords",
+            compared_to="fake"
+            )
+        Product.objects.create(
+            name='test product',
+            brands='test brand',
+            tags='fake, test',
+            ingredients='test, test',
+            additives='E111, E222',
+            allergens='test',
+            labels='test label',
+            stores='super test',
+            link="https://test-product.html",
+            nutriscore="a",
+            image_url='http://test.jpg',
+            keywords="test try",
+            compared_to="test"
+            )
+
+    def test_create_account_and_add_fav(self):
+        self.driver.get(self.live_server_url)
+        self.assertIn('Pur Beurre', self.driver.title)
+
+        create_account = self.driver.find_element_by_id('create')
+        ActionChains(self.driver).click(create_account).perform()
+        self.wait.until(lambda driver:
+                        self.driver.find_element_by_id("submit_new_user"))
+
+        assert self.driver.current_url.endswith('/substitute/signup/')
+
+        username = self.driver.find_element_by_id('id_username')
+        email = self.driver.find_element_by_id('id_email')
+        password1 = self.driver.find_element_by_id('id_password1')
+        password2 = self.driver.find_element_by_id('id_password2')
+        submit = self.driver.find_element_by_id('submit_new_user')
+
+        username.send_keys('fake-user')
+        email.send_keys('fake@foo.bar')
+        password1.send_keys('nofxat411')
+        password2.send_keys('nofxat411')
+        submit.send_keys(Keys.RETURN)
+
+        self.wait.until(lambda driver:
+                        self.driver.find_element_by_id("search_input"))
+
+        assert self.driver.current_url == '{}/'.format(self.live_server_url)
+
+        search = self.driver.find_element_by_id('search_input')
+        submit = self.driver.find_element_by_id('submit_search')
+
+        search.send_keys('fake keywords')
+        submit.send_keys(Keys.RETURN)
+
+        self.wait.until(lambda driver:
+                        self.driver.find_element_by_id("substitutes"))
+
+        assert self.driver.current_url.endswith(
+            '/substitute/results/fake+keywords/')
+
+        self.driver.find_element_by_class_name("add-fav").click()
+        self.wait.until(lambda driver:
+                        self.driver.find_element_by_id("myfav"))
+        myfav_link = self.driver.find_element_by_id("myfav")
+        ActionChains(self.driver).click(myfav_link).perform()
+
+        self.wait.until(lambda driver:
+                        self.driver.find_element_by_id("favorites"))
+
+        assert self.driver.current_url.endswith('/substitute/favorites/')
